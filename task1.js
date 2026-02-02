51e817162b514a5b7e23c2a6334e1423bee9704c
AforArdi <binsbro36@gmail.com>
2026-02-03 00:32:31 +0600

    task 1

---
+++ task1.js
@@ -0,0 +1,8 @@
+// Write a function to convert temperature from Celsius to Fahrenheit.
+
+function cToF(c) {
+    return (c * 9 / 5) + 32;
+}
+
+const celciusValue = 50;
+console.log(cToF(celciusValue));
